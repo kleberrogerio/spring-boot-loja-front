b3ab0770fe01dff3f20d691bb7379fd59ab39b65
kleberrogerio <kleber.nascimento5@fatec.sp.gov.br>
2021-12-04 02:12:02 +0000

    Interceptor

--- src/main.js
+++ src/main.js
@@ -4,6 +4,26 @@ import router from './router'
 import store from './store'
 import axios from 'axios'
 
+axios.interceptors.request.use(config => {
+    if(store.state.token) {
+    config.headers.Authorization = store.state.token
+    }
+    return config
+   })
+   axios.interceptors.response.use(res => {
+    return res
+    }, error => {
+    if(error.response.status === 403) {
+    alert('Não autorizado!')
+    }
+    else if (error.response.status === 401) {
+    store.commit('logout')
+    router.push('/login')
+    }
+    throw error
+   })
+   
+
 axios.defaults.baseURL='https://8080-gray-anaconda-zu06gdet.ws-us21.gitpod.io/spring-loja'
 
 createApp(App).use(store).use(router).mount('#app')
